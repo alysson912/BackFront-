10426a41e11343528f427f75e5a31046928e36e5
alysson912 <almenezes912@gmail.com>
2023-11-17 10:34:31 -0300

    criando camadas de Network

---
+++ ProtocolsCodable/ProtocolsCodable/src/Features/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewModel.swift
+//  ProtocolsCodable
+//
+//  Created by Alysson Menezes on 17/11/23.
+//
+
+import UIKit
+
+class HomeViewModel {
+
+    public func fetchRequest(){
+        
+    }
+}
---
+++ ProtocolsCodable/ProtocolsCodable/src/Network/Service/HomeService.swift
@@ -0,0 +1,12 @@
+//
+//  HomeService.swift
+//  ProtocolsCodable
+//
+//  Created by Alysson Menezes on 17/11/23.
+//
+
+import UIKit
+
+class HomeService: NSObject {
+
+}
